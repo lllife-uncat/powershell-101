a3ca05fe9072abdbb170e129ae2f9ec847505991
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-10-21 11:18:08 +0700

    test remove script

---
+++ remove7.ps1
@@ -0,0 +1,8 @@
+﻿$location = "C:\wk\test"
+$back = 7
+$now = Get-Date
+$files = Get-ChildItem $location -File -Recurse -Filter "*.pdf" `
+    | where { ( $now - $_.CreationTime).TotalDays -gt $back }
+
+Write-Host "Total files -> "  $files.Count
+$files | % { Write-Host $_.FullName; $_.Delete() }
